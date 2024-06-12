cee794367ff31aded1dd0c031d87f515ebaca1ee
Yash020405 <yashaga0204@gmail.com>
2024-06-12 18:58:54 +0530

    Project_Page_content

--- src/Pages/ProjectsPage/Projects.jsx
+++ src/Pages/ProjectsPage/Projects.jsx
@@ -1,69 +1,70 @@
 import React from 'react';
 import './Projects.css';
 import { FaGithub } from 'react-icons/fa';
+import Image_Editor from '../../Assets/Image_Editor.jpeg';
+import Web_Scrapper from '../../Assets/Web_Scrapper.jpeg';
+import Music_Player from '../../Assets/Music_Player.jpeg';
+import Yulu from '../../Assets/Yulu.jpeg';
+import Aerofit from '../../Assets/Aerofit.jpeg';
+import Portfolio from '../../Assets/Portfolio.jpeg';
+import Quiz from '../../Assets/Quiz.jpeg';
+import Service from '../../Assets/Service_Website.jpeg';
 
 const projectsData = [
   {
     id: 1,
     title: 'Image Editor',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius nibh eget ex tincidunt, in mattis justo scelerisque.',
-    imageUrl: 'https://via.placeholder.com/150',
-    githubUrl: 'https://github.com/project1',
+    description: 'A comprehensive image editing tool offering a variety of effects, and adjustment options to enhance your photos.',
+    imageUrl: Image_Editor,
+    githubUrl: 'https://github.com/Yash020405/ImageEditor',
   },
   {
     id: 2,
     title: 'Web Scrapper',
-    description: 'Pellentesque vel sem nec sem auctor blandit. Mauris condimentum ipsum vel est fermentum, vel viverra nulla efficitur.',
-    imageUrl: 'https://via.placeholder.com/150',
-    githubUrl: 'https://github.com/project2',
+    description: 'A powerful application designed to extract data from various websites, enabling users to compare product prices across different online platforms.',
+    imageUrl: Web_Scrapper,
+    githubUrl: 'https://github.com/Yash020405/Web_Scrapper',
   },
   {
     id: 3,
     title: 'Portfolio',
-    description: 'Curabitur lacinia mi at purus condimentum, at efficitur dolor vehicula.',
-    imageUrl: 'https://via.placeholder.com/150',
+    description: ' A personal portfolio website that showcases my projects, skills, and experiences, with an elegant and responsive design.',
+    imageUrl: Portfolio,
     githubUrl: 'https://github.com/Yash020405/portfolio',
   },
   {
     id: 4,
-    title: 'Patient Record System',
-    description: 'Nam vehicula urna sit amet nisi bibendum, sed consectetur tortor fermentum.',
-    imageUrl: 'https://via.placeholder.com/150',
-    githubUrl: 'https://github.com/project4',
-  },
-  {
-    id: 5,
     title: 'Music Player',
-    description: 'Sed ac sapien sed velit sollicitudin luctus. Nulla facilisi.',
-    imageUrl: 'https://via.placeholder.com/150',
+    description: 'A versatile music player that allows users to listen to music, adjust volume, skip tracks forward and backward, and more.',
+    imageUrl: Music_Player,
     githubUrl: 'https://github.com/Yash020405/Music_Player',
   },
   {
-    id: 6,
-    title: 'Quiz App',
+    id: 5,
+    title: 'Quiz Website',
     description: 'Suspendisse potenti. Donec mattis, nisl at scelerisque aliquet, nisi libero viverra quam.',
-    imageUrl: 'https://via.placeholder.com/150',
+    imageUrl: Quiz,
     githubUrl: 'https://github.com/Yash020405/Quiz',
   },
   {
-    id: 7,
+    id: 6,
     title: 'Yulu Data Analysis',
-    description: 'Suspendisse potenti. Donec mattis, nisl at scelerisque aliquet, nisi libero viverra quam.',
-    imageUrl: 'https://via.placeholder.com/150',
-    githubUrl: 'https://github.com/project6',
+    description: 'A data analysis tool that processes and visualizes Yulu bike-sharing data to provide insights into usage patterns and trends.',
+    imageUrl: Yulu,
+    githubUrl: 'https://github.com/Yash020405/Yulu_Analysis',
+  },
+  {
+    id: 7,
+    title: 'Aerofit Data Analysis',
+    description: 'A comprehensive data analysis tool for the Aerofit company, providing insights and recommendations based on fitness data to improve business strategies.',
+    imageUrl: Aerofit,
+    githubUrl: 'https://github.com/Yash020405/Aerofit_Analysis',
   },
   {
     id: 8,
-    title: 'Aerofit Data Analysis',
-    description: 'Suspendisse potenti. Donec mattis, nisl at scelerisque aliquet, nisi libero viverra quam.',
-    imageUrl: 'https://via.placeholder.com/150',
-    githubUrl: 'https://github.com/project6',
-  },
-  {
-    id: 9,
     title: 'Service Website',
-    description: 'Suspendisse potenti. Donec mattis, nisl at scelerisque aliquet, nisi libero viverra quam.',
-    imageUrl: 'https://via.placeholder.com/150',
+    description: 'A landing page designed to showcase services, featuring an attractive layout and essential information for visitors.',
+    imageUrl: Service,
     githubUrl: 'https://github.com/Yash020405/Web_Dev_1',
   },
 ];
